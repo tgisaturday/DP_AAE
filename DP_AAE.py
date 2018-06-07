2f616219fee825fa2abe72bcf314340deb73b313
tgisaturday <jamesk1228@gmail.com>
2018-06-08 00:27:24 +0900

    first commit

---
+++ DP_AAE.py
@@ -0,0 +1,146 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.gridspec as gridspec
+import os
+
+mb_size = 32
+X_dim = 784
+z_dim = 10
+h_dim = 128
+
+num_hidden_1 = 256 # 1st layer num features
+num_hidden_2 = 128 # 2nd layer num features (the latent dim)
+num_input = 784 # MNIST data input (img shape: 28*28)
+
+mnist = input_data.read_data_sets('./MNIST_data', one_hot=True)
+
+def plot(samples):
+    fig = plt.figure(figsize=(4, 4))
+    gs = gridspec.GridSpec(8, 8)
+    gs.update(wspace=0.05, hspace=0.05)
+        
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+
+    return fig
+
+
+
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random_normal(shape=size, stddev=xavier_stddev)
+
+X = tf.placeholder(tf.float32, shape=[None, X_dim])
+
+D_W1 = tf.Variable(xavier_init([X_dim, h_dim]))
+D_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+
+D_W2 = tf.Variable(xavier_init([h_dim, 1]))
+D_b2 = tf.Variable(tf.zeros(shape=[1]))
+
+theta_D = [D_W1, D_W2, D_b1, D_b2]
+
+
+z = tf.placeholder(tf.float32, shape=[None, z_dim])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1])),
+    'encoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2])),
+    'decoder_h1': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1])),
+    'decoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_input])),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([num_hidden_1])),
+    'encoder_b2': tf.Variable(tf.random_normal([num_hidden_2])),
+    'decoder_b1': tf.Variable(tf.random_normal([num_hidden_1])),
+    'decoder_b2': tf.Variable(tf.random_normal([num_input])),
+}
+
+#def sample_z(m, n):
+    #return np.random.uniform(-1., 1., size=[m, n])
+
+def encoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
+                                   biases['encoder_b1']))
+    # Encoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+                                   biases['encoder_b2']))
+    return layer_2
+
+def decoder(x):
+    # Decoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+                                   biases['decoder_b1']))
+    # Decoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+                                   biases['decoder_b2']))
+    return layer_2
+
+def discriminator(x):
+    D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
+    out = tf.matmul(D_h1, D_W2) + D_b2
+    return out
+
+#G_sample = generator(z)
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+# Prediction
+G_sample = decoder_op
+
+D_real = discriminator(X)
+D_fake = discriminator(G_sample)
+
+D_loss = tf.reduce_mean(D_real) - tf.reduce_mean(D_fake)
+G_loss = -tf.reduce_mean(D_fake)
+
+D_solver = (tf.train.RMSPropOptimizer(learning_rate=1e-4)
+            .minimize(-D_loss, var_list=theta_D))
+G_solver = (tf.train.RMSPropOptimizer(learning_rate=1e-4)
+            .minimize(G_loss))
+
+clip_D = [p.assign(tf.clip_by_value(p, -0.01, 0.01)) for p in theta_D]
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+if not os.path.exists('out/'):
+    os.makedirs('out/')
+
+i = 0
+
+for it in range(1000000):
+    for _ in range(5):
+        X_mb, _ = mnist.train.next_batch(mb_size)
+
+        _, D_loss_curr, _ = sess.run(
+            [D_solver, D_loss, clip_D],
+            feed_dict={X: X_mb}
+        )
+
+    _, G_loss_curr = sess.run(
+        [G_solver, G_loss],
+        feed_dict={X: X_mb}
+    )
+
+    if it % 100 == 0:
+        print('Iter: {}; D loss: {:.4}; G_loss: {:.4}'
+              .format(it, D_loss_curr, G_loss_curr))
+
+        if it % 1000 == 0:
+            samples = sess.run(G_sample, feed_dict={X: X_mb})
+            fig = plot(np.append(X_mb, samples, axis=0))
+            plt.savefig('out/{}.png'
+                        .format(str(i).zfill(3)), bbox_inches='tight')
+            i += 1
+            plt.close(fig)
