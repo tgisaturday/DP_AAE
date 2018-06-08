1b7081c9e618537eb17baed27251fb06429eb69e
tgisaturday <jamesk1228@gmail.com>
2018-06-08 20:21:07 +0900

    add gaussian noise to z

--- DP_DCAAE.py
+++ DP_DCAAE.py
@@ -72,6 +72,8 @@ def autoencoder(x):
     # %%
     # store the latent representation
     z = current_input
+    noise = tf.random_normal(shape=tf.shape(z), mean=0.0, stddev=0.02, dtype=tf.float32) 
+    z += noise
     encoder.reverse()
     shapes.reverse()
 
@@ -92,10 +94,18 @@ def autoencoder(x):
     y = current_input
 
     return y
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random_normal(shape=size, stddev=xavier_stddev)
 
 D_W1 = tf.Variable(tf.random_uniform([4,4,1,64],-1.0 ,1.0), name='W1')
 D_W2 = tf.Variable(tf.random_uniform([4,4,64,128], -1.0/8,1.0/8), name='W2')
-
+D_fc1 = tf.Variable(xavier_init([7*7*128, 1024]))
+D_b1 = tf.Variable(tf.zeros(shape=[1024]))
+D_fc2 = tf.Variable(xavier_init([1024,1]))
+D_b2 = tf.Variable(tf.zeros(shape=[1]))
+theta_D = [D_W1, D_W2,D_fc1,D_fc2, D_b1, D_b2]
 def discriminator(x):
     if len(x.get_shape()) == 2:
         x_dim = np.sqrt(x.get_shape().as_list()[1])
@@ -116,32 +126,24 @@ def discriminator(x):
     conv2 = tf.nn.conv2d(h1, D_W2, strides=[1,2,2,1],padding='SAME')
     conv2 = tf.contrib.layers.batch_norm(conv2,center=True, scale=True,is_training=True)
     h2 = tf.nn.leaky_relu(conv2, 0.2)
-    
+  
     h2 = tf.layers.flatten(h2)
-    h2 = tf.layers.dense(h2, 1024,kernel_initializer = tf.truncated_normal_initializer(mean = 0.0, stddev=0.02))
+
+    h2 = tf.matmul(h2, D_fc1) + D_b1
     h2 = tf.contrib.layers.batch_norm(h2,center=True, scale=True,is_training=True)
     h3 = tf.nn.leaky_relu(h2, 0.2)
-    d =  tf.layers.dense(h3,1)
+    
+    d =  tf.matmul(h3, D_fc2) + D_b2
     return d
 
-'''
-def xavier_init(size):
-    in_dim = size[0]
-    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
-    return tf.random_normal(shape=size, stddev=xavier_stddev)
 
+'''
 D_W1 = tf.Variable(xavier_init([X_dim, h_dim]))
 D_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
 
 D_W2 = tf.Variable(xavier_init([h_dim, 1]))
 D_b2 = tf.Variable(tf.zeros(shape=[1]))
-def discriminator(x):
-    if len(x.get_shape()) == 2:
-        x_tensor = x
-    elif len(x.get_shape()) == 4:
-        x_tensor = tf.layers.flatten(x)
-    else:
-        raise ValueError('Unsupported input dimensions')
+
     D_h1 = tf.nn.relu(tf.matmul(x_tensor, D_W1) + D_b1)
     out = tf.matmul(D_h1, D_W2) + D_b2
     return out
@@ -159,25 +161,14 @@ D_loss = tf.reduce_mean(D_real) - tf.reduce_mean(D_fake)
 A_loss = tf.reduce_mean(tf.pow(G_true_flat -G_sample, 2))
 G_loss = -tf.reduce_mean(D_fake)
 
-global_step = tf.Variable(0, name="global_step", trainable=False) 
-#num_batches_per_epoch = int((len(x_train)-1)/256) + 1
-learning_rate = tf.train.exponential_decay(1e-3, global_step,200, 0.95, staircase=True)
-
-optimizer = tf.train.AdamOptimizer(learning_rate=5e-6)
-optimizer_ae = tf.train.AdamOptimizer(learning_rate)
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-D_gradients, D_variables = zip(*optimizer.compute_gradients(-D_loss))
-D_gradients = [tf.clip_by_value(grad, -0.01,0.01) for grad in D_gradients if grad is not None]
-G_gradients, G_variables = zip(*optimizer.compute_gradients(G_loss))
-G_gradients, _ = tf.clip_by_global_norm(G_gradients, 5.0)
-A_gradients, A_variables = zip(*optimizer_ae.compute_gradients(A_loss))
-A_gradients, _ = tf.clip_by_global_norm(A_gradients, 5.0)
+clip_D = [p.assign(tf.clip_by_value(p, -0.01, 0.01)) for p in theta_D]
 
 with tf.control_dependencies(update_ops):
-    D_solver = optimizer.apply_gradients(zip(D_gradients, D_variables), global_step=global_step)
-    G_solver = optimizer.apply_gradients(zip(G_gradients, G_variables), global_step=global_step)
-    A_solver = optimizer.apply_gradients(zip(A_gradients, A_variables), global_step=global_step)
+    D_solver = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(-D_loss, var_list=theta_D)
+    G_solver = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(G_loss)
+    A_solver = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(A_loss)
 
 
 if not os.path.exists('dc_out/'):
@@ -190,7 +181,7 @@ with tf.Session() as sess:
     for it in range(10000000):
         for _ in range(5):
             X_mb, _ = mnist.train.next_batch(mb_size)
-            _, D_loss_curr = sess.run([D_solver, D_loss],feed_dict={X: X_mb})
+            _, D_loss_curr,_ = sess.run([D_solver, D_loss, clip_D],feed_dict={X: X_mb})
         _, A_loss_curr = sess.run([A_solver, A_loss],feed_dict={X: X_mb})
         _, G_loss_curr = sess.run([G_solver, G_loss],feed_dict={X: X_mb})
 
